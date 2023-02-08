cfc26e44a7b078a9489ee6a079e1a6e21fbd7d6e
piwer-m <120250862+msludos@users.noreply.github.com>
2023-02-08 21:52:18 +0300

    News

--- api/index.py
+++ api/index.py
@@ -1,51 +1,55 @@
 from flask import Flask, render_template, url_for
 import os, requests
 
-def get_host():
+async def get_host():
     host = os.environ.get("HOST")
+    return "https://OOVCbot.msludos.repl.co"
 
-def get_token():
-    api = os.environ.get("API")
+async def get_news():
+    url = await get_host()+"/api/news"
+    news = requests.get(url).json()
+    r = []
+
+    for n in news:
+        if n["id"] > 1250:
+            r.append(n)
+
+    return r
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template("index.html", host=get_host())
+async def index():
+    return render_template("index.html")
 
 @app.route('/map')
-def map():
+async def map():
     return render_template("map/index.html")
 
 @app.route('/map/accept')
-def accept():
+async def accept():
     return render_template("map/accept.html")
 
 @app.route('/countries')
-def countries():
+async def countries():
     return render_template("countries/index.html")
 
 @app.route('/countries/<id>')
-def country(id):
+async def country(id):
     return render_template("countries/country.html", id=id)
 
 @app.route('/faq')
-def faq():
+async def faq():
     return render_template("faq/index.html")
 
 @app.route('/news')
-def news():
-    params = (
-        ('v', '5.107'),
-        ('access_token', get_token()),
-        ('owner_id', "")
-    )
-    response = requests.get('https://api.vk.com/method/wall.get?', params=params) 
-    return render_template("news/index.html")
+async def news():
+    news_j = await get_news()
+    return render_template("news/index.html", len=len(news_j), news=news_j)
 
 @app.route('/news/<id>')
-def new(id):
+async def new(id):
     return render_template("news/news.html")
 
 
