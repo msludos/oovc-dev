f8c9656a462dd8b337e297052d5618612c4fdf7d
piwer-m <120250862+msludos@users.noreply.github.com>
2023-02-08 22:00:20 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, url_for
 import os, requests
 
-async def get_host():
+def get_host():
     host = os.environ.get("HOST")
     return "https://OOVCbot.msludos.repl.co"
 
-async def get_news():
-    url = await get_host()+"/api/news"
+def get_news():
+    url = get_host()+"/api/news"
     news = requests.get(url).json()
     r = []
 
@@ -20,36 +20,36 @@ app = Flask(__name__)
 
 
 @app.route('/')
-async def index():
+def index():
     return render_template("index.html")
 
 @app.route('/map')
-async def map():
+def map():
     return render_template("map/index.html")
 
 @app.route('/map/accept')
-async def accept():
+def accept():
     return render_template("map/accept.html")
 
 @app.route('/countries')
-async def countries():
+def countries():
     return render_template("countries/index.html")
 
 @app.route('/countries/<id>')
-async def country(id):
+def country(id):
     return render_template("countries/country.html", id=id)
 
 @app.route('/faq')
-async def faq():
+def faq():
     return render_template("faq/index.html")
 
 @app.route('/news')
-async def news():
-    news_j = await get_news()
+def news():
+    news_j = get_news()
     return render_template("news/index.html", len=len(news_j), news=news_j)
 
 @app.route('/news/<id>')
-async def new(id):
+def new(id):
     return render_template("news/news.html")
 
 
